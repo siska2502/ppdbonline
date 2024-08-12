464706b19fda984b011197e1303f1308dce844d3
sika2502 <siska20si@mahasiswa.pcr.ac.id>
2024-08-12 23:02:26 +0700

    gas keun

--- application/controllers/Panel_admin.php
+++ application/controllers/Panel_admin.php
@@ -3,7 +3,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 
 class Panel_admin extends CI_Controller
 {
-
 	public function index()
 	{
 		$sess = $this->session->userdata('id_admin');
